512d1be5c43aa8d8614c753899870c702df3bafc
vnguyen629 <brotherflab@gmail.com>
2017-05-05 12:45:08 -0700

    Test commit, added Equipment class.

---
+++ AdventureGame/src/edu/orangecoastcollege/cs272/model/Equipment.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.orangecoastcollege.cs272.model;
+
+/** 
+ * The <code></code> class 
+ *
+ * @author Vincent Nguyen
+ * @version 1.0
+*/
+public abstract class Equipment {
+
+}
